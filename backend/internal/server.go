c092eb410b70526ef2b8abca3a71a6e3ce603240
kiioong94 <kiioong94@gmail.com>
2025-03-09 11:54:38 +0100

    add postgres db to stack

--- backend/internal/server.go
+++ backend/internal/server.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"os"
 
 	auth "github.com/kiioong/are_they_playing/gen/go/kiioong/authentication"
 	authenticationService "github.com/kiioong/are_they_playing/internal/AuthenticationService"
 	"google.golang.org/grpc"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := fmt.Sprintf("host=localhost user=%s password=%s dbname=%s port=5432 sslmode=disable", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
 	lis, err := net.Listen("tcp", ":9090")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
