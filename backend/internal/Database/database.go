5bff0c1c5a0337c2f1b824f1f5c90153a5386b52
kiioong94 <kiioong94@gmail.com>
2025-03-16 09:27:52 +0100

    outline basic data structure

--- backend/internal/Database/database.go
+++ backend/internal/Database/database.go
@@ -3,17 +3,12 @@ package Database
 import (
 	"log"
 	"os"
+	"time"
 
 	hash "github.com/kiioong/are_they_playing/internal/Hash"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
-
-type User struct {
-	gorm.Model
-	Username string `gorm:"unique"`
-	Password string
-}
 
 var DB *gorm.DB
 
@@ -24,7 +19,7 @@ func InitDatabase(dsn string) {
 		panic("failed to connect database")
 	}
 
-	DB.AutoMigrate(&User{})
+	DB.AutoMigrate(&User{}, &League{}, &Team{}, &Sport{}, &UserTeam{}, &Game{})
 
 	password, err := hash.HashPassword(os.Getenv("ADMIN_PASSWORD"))
 	if err != nil {
@@ -33,4 +28,53 @@ func InitDatabase(dsn string) {
 	}
 
 	DB.Create(&User{Username: "admin", Password: password})
+}
+
+type User struct {
+	ID        uint      `gorm:"primaryKey"`
+	Username  string    `gorm:"type:varchar(255);unique;not null"`
+	Password  string    `gorm:"type:varchar(255);not null"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+
+	Teams []Team `gorm:"many2many:user_teams;"`
+}
+
+type League struct {
+	ID      uint   `gorm:"primaryKey"`
+	Name    string `gorm:"type:varchar(255);not null"`
+	SportID uint   `gorm:"not null"`
+
+	Sport Sport
+	Teams []Team `gorm:"foreignKey:LeagueID"`
+}
+
+type Team struct {
+	ID         uint   `gorm:"primaryKey"`
+	Name       string `gorm:"type:varchar(255);not null"`
+	PathToLogo string `gorm:"type:varchar(255)"`
+
+	LeagueID uint
+}
+
+type Sport struct {
+	ID   uint   `gorm:"primaryKey"`
+	Name string `gorm:"type:varchar(255);not null"`
+
+	Leagues []League `gorm:"foreignKey:SportID"`
+}
+
+type UserTeam struct {
+	UserID uint `gorm:"primaryKey"`
+	TeamID uint `gorm:"primaryKey"`
+
+	User User `gorm:"foreignKey:UserID"`
+	Team Team `gorm:"foreignKey:TeamID"`
+}
+
+type Game struct {
+	ID        uint `gorm:"primaryKey"`
+	HomeTeam  Team
+	AwayTeam  Team
+	StartTime time.Time
+	League    League
 }
