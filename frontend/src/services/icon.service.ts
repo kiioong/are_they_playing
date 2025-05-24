0130b3c0ef77515070f26951173f59c5349df8a5
kiioong94 <kiioong94@gmail.com>
2025-05-24 10:03:42 +0200

    fixes icon not being loaded properly

--- frontend/src/services/icon.service.ts
+++ frontend/src/services/icon.service.ts
@@ -8,39 +8,39 @@ export class IconService {
   loadIcon = async (iconName: string): Promise<any> => {
     if (!iconName) return null;
 
-  // If this icon is already being loaded, wait for that promise
-  if (this.loadingIcons[iconName]) {
-    return this.loadingIcons[iconName];
-  }
+    // If this icon is already being loaded, wait for that promise
+    if (await this.loadingIcons[iconName]) {
+      return this.loadingIcons[iconName];
+    }
 
-  // If the icon is in cache, return it immediately
-  if (this.iconCache[iconName]) {
-    return this.iconCache[iconName];
-  }
+    // If the icon is in cache, return it immediately
+    if (this.iconCache[iconName]) {
+      return this.iconCache[iconName];
+    }
 
-  // Otherwise, load the icon and cache it
-  const iconPromise = import(`ionicons/icons/`)
-    .then((module) => {
-      const icon = module[iconName];
-      this.iconCache[iconName] = icon;
-      delete this.loadingIcons[iconName];
-      return icon;
-    })
-    .catch(async (error) => {
-      console.error(`Failed to load icon: ${iconName}`, error);
-      // Load a default fallback icon
-      return import("ionicons/icons").then((module) => {
-        const icon = module.help;
+    // Otherwise, load the icon and cache it
+    const iconPromise = import(`ionicons/icons/`)
+      .then((module) => {
+        const icon = module[iconName];
         this.iconCache[iconName] = icon;
         delete this.loadingIcons[iconName];
         return icon;
+      })
+      .catch(async (error) => {
+        console.error(`Failed to load icon: ${iconName}`, error);
+        // Load a default fallback icon
+        return import("ionicons/icons").then((module) => {
+          const icon = module.help;
+          this.iconCache[iconName] = icon;
+          delete this.loadingIcons[iconName];
+          return icon;
+        });
       });
-    });
 
-  // Store the promise in the loading map
-  this.loadingIcons[iconName] = iconPromise;
-  return iconPromise;
-};
+    // Store the promise in the loading map
+    this.loadingIcons[iconName] = iconPromise;
+    return iconPromise;
+  };
 
   // Function to get an icon (will return immediately if cached, or null during loading)
   getIcon = async (iconName: string): Promise<any> => {
